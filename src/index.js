957648ed27729e5dafada8d9ab10475a64ebeef9
Nirvean <snirvean@gmail.com>
2023-07-16 16:11:28 +0200

    Remove Github link

--- src/index.js
+++ src/index.js
@@ -16,17 +16,23 @@ function showSearchResult(response) {
   initialWind.innerHTML = Math.round(response.data.wind.speed);
 
   let initialFeelsLike = document.querySelector("#current-feels-like");
-  initialFeelsLike.innerHTML = `Feels like ${Math.round(response.data.main.feels_like)} ºC`;
+  initialFeelsLike.innerHTML = `Feels like ${Math.round(
+    response.data.main.feels_like
+  )} ºC`;
 
   let initialMaxTemp = document.querySelector("#current-max-temp");
-  initialMaxTemp.innerHTML = `Max.: ${Math.round(response.data.main.temp_max)} ºC ↑`;
+  initialMaxTemp.innerHTML = `Max.: ${Math.round(
+    response.data.main.temp_max
+  )} ºC ↑`;
 
   let initialMinTemp = document.querySelector("#current-min-temp");
-  initialMinTemp.innerHTML = `Min.: ${Math.round(response.data.main.temp_min)} ºC ↓`;
+  initialMinTemp.innerHTML = `Min.: ${Math.round(
+    response.data.main.temp_min
+  )} ºC ↓`;
 
   audioPause(); //To reset music button when searching for a new location
 
-  let tempSymbol = document.querySelector(".fahrenheit-symbol");  //To reset conversion button when searching for a new location
+  let tempSymbol = document.querySelector(".fahrenheit-symbol"); //To reset conversion button when searching for a new location
   if (tempSymbol.innerHTML === "ºC") {
     changeTempType();
   }
@@ -62,10 +68,10 @@ function showSearchResult(response) {
   } else if (initialConditions.textContent === "Tornado") {
     initialWeatherIcon.setAttribute("class", "fa-solid fa-tornado");
   } else if (initialConditions.textContent === "Clear") {
-    if((hourNow >= 21) || (hourNow < 06)) {
+    if (hourNow >= 21 || hourNow < 6) {
       initialWeatherIcon.setAttribute("class", "fa-solid fa-moon");
     } else {
-    initialWeatherIcon.setAttribute("class", "fa-solid fa-sun");
+      initialWeatherIcon.setAttribute("class", "fa-solid fa-sun");
     }
   } else if (initialConditions.textContent === "Clouds") {
     initialWeatherIcon.setAttribute("class", "fa-solid fa-cloud");
@@ -74,16 +80,31 @@ function showSearchResult(response) {
   //Change of music track depending on the parts of the day
   let audioHTMLSelector = document.querySelector("#audio");
 
-  if ((hourNow >= 21) || (hourNow < 06)) {
-    audioHTMLSelector.setAttribute("src", "https://audio.jukehost.co.uk/P079LVYjwprIupeNpCRW0CrFzZYU60KW"); //night
-  } else if ((hourNow >= 06) && (hourNow < 12)) {
-    audioHTMLSelector.setAttribute("src", "https://audio.jukehost.co.uk/3TzpVF4v4y5wdWThMKD3FCCVVS7oQQae"); //morning
-  } else if ((hourNow >= 12) && (hourNow < 13)) {
-    audioHTMLSelector.setAttribute("src", "https://audio.jukehost.co.uk/JNrrMdpABExr5kNeMDrGbTXQ82ZPNdMV"); //noon
-  } else if ((hourNow >= 13) && (hourNow < 17)) {
-    audioHTMLSelector.setAttribute("src", "https://audio.jukehost.co.uk/QeMcZD4Axh76BZWaUtZkWNhykbEJTmBI"); //afternoon
-  } else if ((hourNow >= 17) && (hourNow < 21)) {
-    audioHTMLSelector.setAttribute("src", "https://audio.jukehost.co.uk/AD4k3WmtONihvYMSA2oHgqTWFGLfzHk8"); //evening
+  if (hourNow >= 21 || hourNow < 6) {
+    audioHTMLSelector.setAttribute(
+      "src",
+      "https://audio.jukehost.co.uk/P079LVYjwprIupeNpCRW0CrFzZYU60KW"
+    ); //night
+  } else if (hourNow >= 6 && hourNow < 12) {
+    audioHTMLSelector.setAttribute(
+      "src",
+      "https://audio.jukehost.co.uk/3TzpVF4v4y5wdWThMKD3FCCVVS7oQQae"
+    ); //morning
+  } else if (hourNow >= 12 && hourNow < 13) {
+    audioHTMLSelector.setAttribute(
+      "src",
+      "https://audio.jukehost.co.uk/JNrrMdpABExr5kNeMDrGbTXQ82ZPNdMV"
+    ); //noon
+  } else if (hourNow >= 13 && hourNow < 17) {
+    audioHTMLSelector.setAttribute(
+      "src",
+      "https://audio.jukehost.co.uk/QeMcZD4Axh76BZWaUtZkWNhykbEJTmBI"
+    ); //afternoon
+  } else if (hourNow >= 17 && hourNow < 21) {
+    audioHTMLSelector.setAttribute(
+      "src",
+      "https://audio.jukehost.co.uk/AD4k3WmtONihvYMSA2oHgqTWFGLfzHk8"
+    ); //evening
   }
 
   //Runs the five-day forecast
@@ -97,53 +118,53 @@ axios.get(apiUrl).then(showSearchResult);
 
 //Current local date and hour
 function showLocalDateAndHour(response) {
-let currentDate = document.querySelector("#current-local-date");
-let currentTime = document.querySelector("#current-local-hour");
+  let currentDate = document.querySelector("#current-local-date");
+  let currentTime = document.querySelector("#current-local-hour");
 
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday"
-];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
-let months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December"
-];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
 
-let timeNow = new Date();
-let cityInfo = response.data;
-let timezone = cityInfo.timezone;
-let localTime = timeNow.getTime();
-let localOffset = timeNow.getTimezoneOffset() * 60000;
-let utc = localTime + localOffset;
-let cityDateCode = utc + 1000 * timezone;
-let cityDate = new Date (cityDateCode);
-let dayNow = days[cityDate.getDay()];
-let monthNow = months[cityDate.getMonth()];
-let dateNow = cityDate.getDate();
-let hourNow = cityDate.getHours();
-hourNow = ("0" + hourNow).slice(-2); //or if (hourNow < 10) {hours = `0${hourNow}`;}
-let minutesNow = timeNow.getMinutes();
-minutesNow = ("0" + minutesNow).slice(-2); //or if (minutesNow < 10) {hours = `0${minutesNow}`;}
+  let timeNow = new Date();
+  let cityInfo = response.data;
+  let timezone = cityInfo.timezone;
+  let localTime = timeNow.getTime();
+  let localOffset = timeNow.getTimezoneOffset() * 60000;
+  let utc = localTime + localOffset;
+  let cityDateCode = utc + 1000 * timezone;
+  let cityDate = new Date(cityDateCode);
+  let dayNow = days[cityDate.getDay()];
+  let monthNow = months[cityDate.getMonth()];
+  let dateNow = cityDate.getDate();
+  let hourNow = cityDate.getHours();
+  hourNow = ("0" + hourNow).slice(-2); //or if (hourNow < 10) {hours = `0${hourNow}`;}
+  let minutesNow = timeNow.getMinutes();
+  minutesNow = ("0" + minutesNow).slice(-2); //or if (minutesNow < 10) {hours = `0${minutesNow}`;}
 
-currentDate.innerHTML = `${dayNow}, ${monthNow} ${dateNow}`;
-currentTime.innerHTML = `${hourNow}:${minutesNow}`;
+  currentDate.innerHTML = `${dayNow}, ${monthNow} ${dateNow}`;
+  currentTime.innerHTML = `${hourNow}:${minutesNow}`;
 
-return (hourNow);
+  return hourNow;
 }
 
 //Search engine
@@ -157,19 +178,21 @@ function searchCity(event) {
   let h2 = document.querySelector("#current-city");
   let apiKey = "b4966aaefe805e530bcf3948c7f52bbe";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apiKey}&units=metric`;
-  if(!(axios.get(apiUrl)
-  .catch((error) => {
-    swal.fire({ //Resetti alert for invalid location
-      imageUrl: "https://i.imgur.com/45Ydv3s.png",
-      title: "...WHAT WAS THAT?!",
-      html: "Before hitting the search button, type in a VALID location! Type in a VALID one! I KNOW you knew that. <br> <br> YA HEAR ME? <br> <br> <strong>NOW, SCRAM!</strong>",
-      confirmButtonText: "Okay...",
-      confirmButtonColor: "#6bb888",
-      width: 500,
-      height: 400,
-      allowOutsideClick: false
-    });
-  }))){
+  if (
+    !axios.get(apiUrl).catch((error) => {
+      swal.fire({
+        //Resetti alert for invalid location
+        imageUrl: "https://i.imgur.com/45Ydv3s.png",
+        title: "...WHAT WAS THAT?!",
+        html: "Before hitting the search button, type in a VALID location! Type in a VALID one! I KNOW you knew that. <br> <br> YA HEAR ME? <br> <br> <strong>NOW, SCRAM!</strong>",
+        confirmButtonText: "Okay...",
+        confirmButtonColor: "#6bb888",
+        width: 500,
+        height: 400,
+        allowOutsideClick: false,
+      });
+    })
+  ) {
     h2.innerHTML = `${searchInput.value}`;
   }
   axios.get(apiUrl).then(showSearchResult);
@@ -205,44 +228,44 @@ currentLocationButton.addEventListener("click", readCurrentLocation);
 
 //Main Celsius to Fahrenheit conversion (included maximum temperature, minimum temperature and feels like temperature conversion)
 function showTempInFahrenheit(response) {
-let showFahrenheit = Math.round(response.data.main.temp);
-let initialTemp = document.querySelector("#current-temp");
-initialTemp.innerHTML = `${showFahrenheit} ºF`;
+  let showFahrenheit = Math.round(response.data.main.temp);
+  let initialTemp = document.querySelector("#current-temp");
+  initialTemp.innerHTML = `${showFahrenheit} ºF`;
 
-let showFeelsLikeInFahrenheit = Math.round(response.data.main.feels_like);
-let initialFeelsLike = document.querySelector("#current-feels-like");
-initialFeelsLike.innerHTML = `Feels like ${showFeelsLikeInFahrenheit} ºF`;
+  let showFeelsLikeInFahrenheit = Math.round(response.data.main.feels_like);
+  let initialFeelsLike = document.querySelector("#current-feels-like");
+  initialFeelsLike.innerHTML = `Feels like ${showFeelsLikeInFahrenheit} ºF`;
 
-let showMaxTempInFahrenheit = Math.round(response.data.main.temp_max);
-let initialMaxTemp = document.querySelector("#current-max-temp");
-initialMaxTemp.innerHTML = `Max.: ${showMaxTempInFahrenheit} ºF ↑`;
+  let showMaxTempInFahrenheit = Math.round(response.data.main.temp_max);
+  let initialMaxTemp = document.querySelector("#current-max-temp");
+  initialMaxTemp.innerHTML = `Max.: ${showMaxTempInFahrenheit} ºF ↑`;
 
-let showMinTempInFahrenheit = Math.round(response.data.main.temp_min);
-let initialMinTemp = document.querySelector("#current-min-temp");
-initialMinTemp.innerHTML = `Min.: ${showMinTempInFahrenheit} ºF ↓`;
+  let showMinTempInFahrenheit = Math.round(response.data.main.temp_min);
+  let initialMinTemp = document.querySelector("#current-min-temp");
+  initialMinTemp.innerHTML = `Min.: ${showMinTempInFahrenheit} ºF ↓`;
 
-getForecast(response.data.coord); // Change five-day forecast to Fahrenheit
+  getForecast(response.data.coord); // Change five-day forecast to Fahrenheit
 }
 
 function showTempInCelsius(response) {
   let showCelsius = Math.round(response.data.main.temp);
   let initialTemp = document.querySelector("#current-temp");
   initialTemp.innerHTML = `${showCelsius} ºC`;
-  
+
   let showFeelsLikeInCelsius = Math.round(response.data.main.feels_like);
   let initialFeelsLike = document.querySelector("#current-feels-like");
   initialFeelsLike.innerHTML = `Feels like ${showFeelsLikeInCelsius} ºC`;
-  
+
   let showMaxTempInCelsius = Math.round(response.data.main.temp_max);
   let initialMaxTemp = document.querySelector("#current-max-temp");
   initialMaxTemp.innerHTML = `Max.: ${showMaxTempInCelsius} ºC ↑`;
-  
+
   let showMinTempInCelsius = Math.round(response.data.main.temp_min);
   let initialMinTemp = document.querySelector("#current-min-temp");
   initialMinTemp.innerHTML = `Min.: ${showMinTempInCelsius} ºC ↓`;
 
-  getForecast(response.data.coord) // Change five-day forecast to Celsius
-  }
+  getForecast(response.data.coord); // Change five-day forecast to Celsius
+}
 
 function changeTempType() {
   let apiKey = "b4966aaefe805e530bcf3948c7f52bbe";
@@ -307,60 +330,64 @@ function showForecastDayName(timestamp) {
 }
 
 function showForecast(response) {
-let forecast = response.data.daily;
+  let forecast = response.data.daily;
 
-let fiveDayForecast = document.querySelector("#forecast");
+  let fiveDayForecast = document.querySelector("#forecast");
 
-let forecastHTML = '<div class="row justify-content-around third-row">';
-forecast.forEach(function (forecastDay, index) {
-  if (index < 5) {
-    let addProperIcon = ``;
-    let forecastIcon = forecastDay.weather[0].main;
-    if (forecastIcon === "Drizzle") {
-      addProperIcon = "fa-solid fa-cloud-rain";
-    } else if (forecastIcon === "Rain") {
-      addProperIcon = "fa-solid fa-cloud-showers-heavy";
-    } else if (forecastIcon === "Snow") {
-      addProperIcon = "fa-regular fa-snowflake";
-    } else if (forecastIcon === "Mist") {
-      addProperIcon = "fa-solid fa-smog";
-    } else if (forecastIcon === "Smoke") {
-      addProperIcon = "fa-solid fa-smog";
-    } else if (forecastIcon === "Haze") {
-      addProperIcon = "fa-solid fa-smog";
-    } else if (forecastIcon === "Dust") {
-      addProperIcon = "fa-solid fa-smog";
-    } else if (forecastIcon === "Fog") {
-      addProperIcon = "fa-solid fa-smog";
-    } else if (forecastIcon === "Sand") {
-      addProperIcon = "fa-solid fa-smog";
-    } else if (forecastIcon === "Ash") {
-      addProperIcon = "fa-solid fa-smog";
-    } else if (forecastIcon === "Squall") {
-      addProperIcon = "fa-solid fa-wind";
-    } else if (forecastIcon === "Tornado") {
-      addProperIcon = "fa-solid fa-tornado";
-    } else if (forecastIcon === "Clear") {
-      addProperIcon = "fa-solid fa-sun";
-    } else if (forecastIcon === "Clouds") {
-      addProperIcon = "fa-solid fa-cloud";
-    }
+  let forecastHTML = '<div class="row justify-content-around third-row">';
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      let addProperIcon = ``;
+      let forecastIcon = forecastDay.weather[0].main;
+      if (forecastIcon === "Drizzle") {
+        addProperIcon = "fa-solid fa-cloud-rain";
+      } else if (forecastIcon === "Rain") {
+        addProperIcon = "fa-solid fa-cloud-showers-heavy";
+      } else if (forecastIcon === "Snow") {
+        addProperIcon = "fa-regular fa-snowflake";
+      } else if (forecastIcon === "Mist") {
+        addProperIcon = "fa-solid fa-smog";
+      } else if (forecastIcon === "Smoke") {
+        addProperIcon = "fa-solid fa-smog";
+      } else if (forecastIcon === "Haze") {
+        addProperIcon = "fa-solid fa-smog";
+      } else if (forecastIcon === "Dust") {
+        addProperIcon = "fa-solid fa-smog";
+      } else if (forecastIcon === "Fog") {
+        addProperIcon = "fa-solid fa-smog";
+      } else if (forecastIcon === "Sand") {
+        addProperIcon = "fa-solid fa-smog";
+      } else if (forecastIcon === "Ash") {
+        addProperIcon = "fa-solid fa-smog";
+      } else if (forecastIcon === "Squall") {
+        addProperIcon = "fa-solid fa-wind";
+      } else if (forecastIcon === "Tornado") {
+        addProperIcon = "fa-solid fa-tornado";
+      } else if (forecastIcon === "Clear") {
+        addProperIcon = "fa-solid fa-sun";
+      } else if (forecastIcon === "Clouds") {
+        addProperIcon = "fa-solid fa-cloud";
+      }
 
-    forecastHTML = forecastHTML + `
+      forecastHTML =
+        forecastHTML +
+        `
     <div id="forecast-day-${index + 1}" class="col-2">
           <div class="row">
             <p>${showForecastDayName(forecastDay.dt)}</p>
           </div>
           <div id="forecast-day" class="row">
             <i id="thu-icon" class="${addProperIcon}"></i>
-            <span id="thu-forecast">${Math.round(forecastDay.temp.max)} º | ${Math.round(forecastDay.temp.min)} º</span>
+            <span id="thu-forecast">${Math.round(
+              forecastDay.temp.max
+            )} º | ${Math.round(forecastDay.temp.min)} º</span>
           </div>
         </div>`;
-  }
-});
+    }
+  });
 
-forecastHTML = forecastHTML + '</div>';
-fiveDayForecast.innerHTML = forecastHTML;
+  forecastHTML = forecastHTML + "</div>";
+  fiveDayForecast.innerHTML = forecastHTML;
 }
 
 function getForecast(coordinates) {
